c4b236e916e45fc053743a13d315a9c8b6d8487e
Victor Hugo <victorhmds@hotmail.com>
2025-04-10 12:57:09 -0300

    Snake moving now!

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.math import Vector2
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.direction = Vector2(1,0)
+    
+    def draw_snake(self):
+        for block in self.body:
+            x = int(block.x * cell_size)
+            y = int(block.y * cell_size)
+            w = h = cell_size
+            snake_rect_size = (x, y, w, h)
+            snake_rect = pygame.Rect(snake_rect_size)
+            pygame.draw.rect(screen, (183,111,122), snake_rect)
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 class FRUIT:
     def __init__(self):
-        self.x = 5
-        self.y = 4
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.x, self.y)
+
     def draw_fruit(self):
-        fruit_rect_size = (x, y, w, h) = self.pos.x, self.pos.y, cell_size, cell_size
+        x = int(self.pos.x * cell_size)
+        y = int(self.pos.y * cell_size)
+        w = h = cell_size
+        fruit_rect_size = (x, y, w, h)
         fruit_rect = pygame.Rect(fruit_rect_size)
         pygame.draw.rect(screen, (126,166,114), fruit_rect)
 
@@ -20,17 +43,11 @@ dimensions = cell_number * cell_size
 screen_size = (width, height) = dimensions, dimensions
 screen = pygame.display.set_mode((screen_size))
 
-# rect_size_draw = x, y, w, h = 100, 200, 50, 50
-# test_rect_draw = pygame.Rect(rect_size)
-
-# surface_size = (width, height) = 100, 200
-# test_surface = pygame.Surface((surface_size))
-# test_rect = test_surface.get_rect(center = (200,250))
-# test_surface.fill('light blue')
-
 fruit = FRUIT()
+snake = SNAKE()
 
-screen.fill((175,215,70))
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 clock = pygame.time.Clock()
 framerate = 60
@@ -40,9 +57,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
-    # pygame.draw.ellipse(screen, pygame.Color('red'), test_rect_draw)
-    # screen.blit(test_surface,test_rect)
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
+                
+    screen.fill((175,215,70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(framerate)
