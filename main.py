6faf64c05d6fc311143f33f018d3ece72f744832
Victor Hugo <victorhmds@hotmail.com>
2025-04-09 22:25:49 -0300

    Second commit

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import pygame, sys
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x, self.y)
+    def draw_fruit(self):
+        fruit_rect_size = (x, y, w, h) = self.pos.x, self.pos.y, cell_size, cell_size
+        fruit_rect = pygame.Rect(fruit_rect_size)
+        pygame.draw.rect(screen, (126,166,114), fruit_rect)
 
 pygame.init()
 
-screen_size = (width, height) = 400, 500
+cell_size = 30
+cell_number = 20
+dimensions = cell_number * cell_size
+
+screen_size = (width, height) = dimensions, dimensions
 screen = pygame.display.set_mode((screen_size))
 
-surface_size = (width, height) = 100, 200
-test_surface = pygame.Surface((surface_size))
+# rect_size_draw = x, y, w, h = 100, 200, 50, 50
+# test_rect_draw = pygame.Rect(rect_size)
 
-# rect_size = x, y, w, h = 100, 200, 50, 50
-# test_rect = pygame.Rect(rect_size)
-test_rect = test_surface.get_rect(center = (200,250))
+# surface_size = (width, height) = 100, 200
+# test_surface = pygame.Surface((surface_size))
+# test_rect = test_surface.get_rect(center = (200,250))
+# test_surface.fill('light blue')
+
+fruit = FRUIT()
 
 screen.fill((175,215,70))
-test_surface.fill('light blue')
 
 clock = pygame.time.Clock()
 framerate = 60
@@ -24,7 +41,8 @@ while True:
             pygame.quit()
             sys.exit()
             
-    # pygame.draw.ellipse(screen, pygame.Color('red'), test_rect)
-    screen.blit(test_surface,test_rect)
+    # pygame.draw.ellipse(screen, pygame.Color('red'), test_rect_draw)
+    # screen.blit(test_surface,test_rect)
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(framerate)
